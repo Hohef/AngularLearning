86559d54a4d3f107a2ba408fa5245c6904ab9359
joseph.locker <joseph.locker@SERN.mil>
2022-08-30 14:34:46 -0400

    Pipes, Bindings, Properties, components, lessons

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from 'src/products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { NgModel } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
